bfd6ca097881c768a43802a5f8adc6902f514aef
cmurestudillos <39341418+cmurestudillos@users.noreply.github.com>
2020-03-25 08:36:02 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@ export const environment = {
   production: false,
   url: 'http://localhost:5000/appgoty-5442a/us-central1',
   firebase: {
-    apiKey: "AIzaSyApw0rVzDaMs5EbGkCm0qC45phlvv8G1Xs",
-    authDomain: "appgoty-5442a.firebaseapp.com",
-    databaseURL: "https://appgoty-5442a.firebaseio.com",
-    projectId: "appgoty-5442a",
-    storageBucket: "appgoty-5442a.appspot.com",
-    messagingSenderId: "186236264202",
-    appId: "1:186236264202:web:778a5da5133e5271fb64f3",
-    measurementId: "G-WC1D4DRBNP"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxx"
   }
 };
 
